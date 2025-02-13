de70b2b286fd0dc55c8d697f4593c0e3c1ef88c2
keikoWei <keiko19970908@gmail.com>
2025-02-14 01:44:50 +0800

    Initial commit

---
+++ src/main/java/com/dane/nkust_oom_backend/NkustOomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dane.nkust_oom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkustOomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkustOomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dane/nkust_oom_backend/NkustOomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dane.nkust_oom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkustOomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
